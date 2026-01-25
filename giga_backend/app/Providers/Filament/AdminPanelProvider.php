cdbb76b61910cc1f7fa60423b0c6e65308b3e690
Your Name <you@example.com>
2026-01-25 00:53:21 -0800

    UI: Restore sidebar navigation groups

--- giga_backend/app/Providers/Filament/AdminPanelProvider.php
+++ giga_backend/app/Providers/Filament/AdminPanelProvider.php
@@ -41,6 +41,11 @@ class AdminPanelProvider extends PanelProvider
             ->sidebarCollapsibleOnDesktop()
             ->sidebarFullyCollapsibleOnDesktop()
             ->maxContentWidth('full')
+            ->navigationGroups([
+                'Operations',
+                'Management',
+                'Settings',
+            ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
