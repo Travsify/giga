48c0398a0415a5d7b973d1c082aeb874cfafac59
Your Name <you@example.com>
2026-01-24 17:27:27 -0800

    feat: add AdminSeeder to provision super admin account on Render

---
+++ giga_backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@giga.com'],
+            [
+                'name' => 'Super Admin',
+                'password' => Hash::make('GigaAdmin2026!'),
+                'role' => 'SuperAdmin',
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
