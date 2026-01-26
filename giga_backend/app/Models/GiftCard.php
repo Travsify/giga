64a1068f2193e6b02d1a71f4bbec47afd13d5f47
Your Name <you@example.com>
2026-01-26 06:20:13 -0800

    Fix GiftCard syntax error

--- giga_backend/app/Models/GiftCard.php
+++ giga_backend/app/Models/GiftCard.php
@@ -6,4 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class GiftCard extends Model
 {
+    protected $fillable = [
+        'code',
+        'amount',
+        'currency_code',
+        'current_uses',
+        'max_uses',
+        'expires_at',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'current_uses' => 'integer',
+        'max_uses' => 'integer',
+        'expires_at' => 'datetime',
+        'is_active' => 'boolean',
+    ];
 }
